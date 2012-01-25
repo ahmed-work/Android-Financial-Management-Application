05f773a52faf963c5f9813c8e555ae6a67be9993
Ahmed Sheikh <ahmed.work@gmail.com>
2012-01-24 22:44:44 -0500

    Initializing Project

---
+++ FinancialManagementApplication/src/android/financialmanagement/FinancialManagementApplicationActivity.java
@@ -0,0 +1,13 @@
+package android.financialmanagement;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FinancialManagementApplicationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
